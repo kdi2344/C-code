232155893408c07dbc3afe5579cbb2ad1d29bb90
Dani Kang <90600217+kdi2344@users.noreply.github.com>
2023-03-21 14:29:23 +0900

    20230315_multiple

---
+++ 20230315_homeWork_multiple/20230315_homeWork_multiple/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230315_homeWork_multiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            Console.Write("출력하고 싶으신 구구단 숫자를 입력해주세요: ");
+            input = int.Parse(Console.ReadLine());
+            int n = 1;
+            while (n < 10)
+            {
+                Console.WriteLine("{0} * {1} = {2}",input, n,  input * n);
+                n++;
+            }
+        }
+    }
+}
